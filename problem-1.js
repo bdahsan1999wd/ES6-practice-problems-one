ce3e6d6d473920d852fe34320550a69014d0df19
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-20 22:23:20 +0600

    problem-2 added and explain the code

--- problem-1.js
+++ problem-1.js
@@ -2,23 +2,28 @@
 // const z = y(5);
 // console.log(z);
 
-/* ------------------------------No 1------------------------------ */
+/* ------------------------------No 1 problem------------------------------ */
 
 /* Write an arrow function that will take 3 parameters, will multiply
 the parameters and will return the result.*/
 
-const Three = (a, b, c) => a * b * c;
-const result1 = Three(2, 3, 4);
-console.log(result1);
+const Three = (a, b, c) => a * b * c;  /* define an arrow function called Three that takes three parameters and returns their product */
 
-/* ------------------------------No 2------------------------------ */
+const result1 = Three(2, 3, 4);  /* call the Three function with arguments 2, 3, and 4 and store the result in a variable called result1 */
+
+console.log(result1);  /* print the value of result1 to the console, which should be 24 (the product of 2, 3, and 4) */
+
+
+/* ------------------------------No 2 problem------------------------------ */
 
 /* Write the following sentence in three lines and print the result:
 I am a web developer. I love to code. I love to eat biryani. */
 
-const line1 = 'I am a web developer.';
-const line2 = 'I love to code.';
-const line3 = 'I love to eat biryani.';
+const line1 = 'I am a web developer.';  /* define a string variable containing the first sentence */
+
+const line2 = 'I love to code.';  /* define a string variable containing the second sentence */
+
+const line3 = 'I love to eat biryani.';  /* define a string variable containing the third sentence */
 
 const result2 = `
 ${line1}
@@ -27,19 +32,28 @@ ${line2}
 
 ${line3}
 `;
-console.log(result2);
 
-/* ------------------------------No 3------------------------------ */
+console.log(result2);  /* print the value of result2, which should output the three sentences on separate lines */
+
+
+/* ------------------------------No 3 problem------------------------------ */
 
 /* Write an arrow function that will take 2 parameters: One parameter
 will come from you and the other parameter will be a default
 parameter. Add these two parameters and return the result. */
 
-const add = (num1, num2 =20) => {
-    const result3 = num1 + num2;
-    return result3;
-  };
-const sum1 = add(5);
-const sum2 = add(5, 10);
-console.log(sum1);
-console.log(sum2);
+const add = (num1, num2 =20) => {  /* define an arrow function called add that takes two parameters, num1 and num2, with num2 having a default value of 20 if no value is passed for num2 */
+
+  const result3 = num1 + num2; /* add the two parameters and store the result in a variable called result3 */
+
+  return result3;  /* return the value of result3 */
+};
+
+const sum1 = add(5);  /* call the add function with one argument and store the result in a variable called sum1 */
+
+const sum2 = add(5, 10);  /* call the add function with two arguments and store the result in a variable called sum2 */
+
+console.log(sum1);  /* print the value of sum1 to the console, which should be 25 (the sum of 5 and the default value of 20 for num2) */
+
+console.log(sum2);  /* print the value of sum2 to the console, which should be 15 (the sum of 5 and 10) */
+
---
+++ problem-2.js
@@ -0,0 +1,29 @@
+/* ------------------------------No 1 problem------------------------------ */
+
+/* Write an arrow function where it will do the following:
+
+a) It will take an array where the array elements will be the
+name of your friends
+b) Check if the length of each element is even, push elements
+with even length to a new array and return the result
+
+*/
+
+const friends = (friendsArray) => {
+    const friendsEvenLength = [];  /* create an empty array to store the friends with even length names */
+  
+    friendsArray.forEach(friend => {  /* loop through each element of the friendsArray using forEach */
+  
+      if (friend.length % 2 === 0) {  /* check if the length of the friend's name is even */
+        friendsEvenLength.push(friend);  /* if it's even, push the friend's name into the friendsEvenLength array */
+      }
+    });
+  
+    return friendsEvenLength;  /* return the array containing the friends with even length names */
+  }
+  
+  const myFriends = ['Md','Ahsan','Habib','Ashik'];
+
+  const friendsLength = friends(myFriends);  /* call the friends function with the myFriends array as argument */
+
+  console.log(friendsLength);  /* log the result to the console, which should be an array containing ['Md', 'Habib', 'Ashik'] */
